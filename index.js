af9d7f60b6ef8a92822b28cc2bd987ad6e94181b
joao amorim <amorimvictorjoao3@gmail.com>
2020-06-15 16:07:17 -0300

    cards main finalizados

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 var dados = {
   noticia: [
     {
-      titulo: "O que é o Antivírus?Entenda mais",
+      titulo: "O que é o Antivírus? Entenda mais",
       categoria: "iniciante",
       url: "https://blog-br.softwareone.com/o-que-e-antivirus",
-      resumo: `Desde que a computação surgiu, pessoas mal intencionadas utilizam conhecimentos em programação para criar códigos maliciosos, que danificam o computador, roubam dados dos usuários, entre outros malefícios. O assunto é sério: alguns programas são capazes até mesmo de captar tudo o que digitamos no teclado. 
-      Por isso, prevenir-se é fundamental.E é justamente para combater essas pragas virtuais que foram criados os antivírus. Mas você sabe o que é antivírus? Vamos desvendar como funcionam esses programas mantidos por grandes empresas de segurança e descobrir algumas das melhores opções disponíveis. Ficou curioso? Então acompanhe!`,
+      img: "/img/informacao/iniciante/antivirus.png",
+      resumo: `Entenda como funciona um antivírus e como ele pode te auxiliar no cotidiano`,
     },
     {
       titulo: "Aplicativo para Gerenciar suas senhas",
       categoria: "iniciante",
       url:
         "https://canaltech.com.br/apps/Aplicativos-para-gerenciar-suas-senhas/",
-      resumo: `Com os hackers cada vez mais ágeis e as ameaças à nossa segurança online crescendo vertiginosamente, as informações de login estão se tornando cada vez mais delicadas e valiosas. Criar senhas realmente seguras exige ao menos uma letra maiúscula, símbolo, número, o nome da tia-avó da sua vizinha do lado esquerdo, e coisas do tipo. Mas como se lembrar de todas essas senhas, uma vez que é indicado usar combinações diferentes para cada login?
-      Felizmente, existem aplicativos que ajudam a aliviar um pouco o estresse de precisar decorar tantas senhas. Eles guardam suas informações de login de forma segura em um único lugar. A maioria dos aplicativos também ajuda a detectar tentativas de phishing e hacking, assim você não precisa se preocupar com invasões ao próprio aplicativo. Confira algumas dicas de apps móveis e para desktop que podem te ajudar com esse dilema da vida moderna.`,
+      img: "img/informacao/iniciante/gerente.jpeg",
+      resumo: `Aplicativos que podem te auxiliar a guardar senhas e logins facilitando assim sua proteção`,
     },
     {
       titulo: "Como organizar documentos para fazer backup?",
       categoria: "iniciante",
       url:
         "https://canaltech.com.br/seguranca/Como-organizar-documentos-para-fazer-backup/",
+      img: "img/informacao/iniciante/backup.png",
       resumo:
-        "Um bom plano de recuperação de desastres envolve mais do que o backup e armazenamento de informações num lugar fora do ambiente físico da empresa. Normalmente a estratégia inclui procedimentos testados e documentados que devem ser seguidos à risca e revisados periodicamente. Afinal, quando a crise se instaura, ninguém quer ser pego de surpresa. Mais do que isso: quando uma empresa está preparada para situações extremas, há redução de perdas importantes, redução de eventuais arranhões à imagem corporativa, redução de interrupções, distribuição de responsabilidades, mais segurança e melhores resultados para os clientes, além de um retorno mais rápido à estabilidade. Mas, saber organizar documentos vem sendo o grande X da questão para a maioria das empresas.",
+        "Dicas para organização na hora de fazer backups em seu computador",
     },
     {
       titulo: "Tudo sobre Phishing",
       categoria: "iniciante",
       url: "https://br.malwarebytes.com/phishing/",
+      img: "img/informacao/iniciante/Phishing.jpg",
       resumo: `O que é phishing? Tipos de phising..Como me proteger? Leia para entender um pouco mais sobre essa técnica de roubo de informações tao utilizadas no dia a dia`,
     },
     {
@@ -34,6 +36,7 @@ var dados = {
       categoria: "iniciante",
       url:
         "https://canaltech.com.br/seguranca/10-praticas-definitivas-para-garantir-a-seguranca-na-internet/",
+      img: "img/informacao/iniciante/praticas.jpg",
       resumo: `Dicas que te guiarão a se manter mais seguro nesse ambiente cada vez mais movimentado que é a web`,
     },
     {
@@ -41,34 +44,38 @@ var dados = {
       categoria: "intermediario",
       url:
         "https://www.kaspersky.com.br/resource-center/threats/botnet-attacks",
+      img: "img/informacao/intermediario/botnet.jpg",
       resumo:
-        "Saiba o que são as botnets e como elas podem afetar seu dia a dia",
+        "Saiba o que são as botnets e como elas podem afetar seu dia a dia.",
     },
     {
       titulo: "Cavalo de Troia",
       categoria: "intermediario",
       url: "https://www.kaspersky.com.br/resource-center/threats/trojans",
+      img: "img/informacao/intermediario/troia.jpg",
       resumo:
-        "Saiba o que é, e mais importante como se proteger desse destruidor vírus",
+        "Saiba o que é, e mais importante como se proteger desse destruidor vírus.",
     },
     {
-      titulo: "Como corrigir os erros java que aparecem no seu computador",
+      titulo: "Como corrigir os erros java que aparecem no seu computador.",
       categoria: "intermediario",
       url:
         "https://canaltech.com.br/software/Como-corrigir-os-erros-Java-que-aparecem-no-seu-computador/",
-      resumo: `A linguagem de programação Java é bastante conhecida e muito utilizada nas várias aplicações que acessamos no nosso dia a dia. Ela está presente também em diversos sites que necessitam de acesso seguro, como, por exemplo, sites de banco. Contudo, o Java torna-se também muito incoveniente quando envia notificações de erro que insistem em aparecer e travar nossos computadores quando menos esperamos ou impedir que possamos acessar sites bancários.
-      Se você é uma vítima que sofre frequentemente com os erros Java, fique tranquilo! Em um simples passo a passo te ensinaremos três formas de solucionar falhas e corrigir os insistentes erros Java que aparecem nos computadores.`,
+      img: "img/informacao/intermediario/java.png",
+      resumo: `A linguagem Java é essêncial para nosso cotidiano mas as vezes leva a aparecer alguns erros, saiba aqui como resolvê-los. `,
     },
     {
       titulo: "O que é OWASP e sua importância para segurança em aplicações",
       categoria: "intermediario",
-      url: "ttps://blog.convisoappsec.com/owasp-e-sua-importancia/",
-      resumo: `A segurança de aplicativos e softwares de computadores é simplesmente uma das etapas mais importantes do planejamento para o desenvolvimento. Afinal, o nível de confiabilidade é o que determinará o sucesso dele, e isso se refletirá no número de usuários ativos no aplicativo, por exemplo. E não tem como falar em segurança sem mencionar o OWASP.`,
+      url: "https://blog.convisoappsec.com/owasp-e-sua-importancia/",
+      img: "img/informacao/intermediario/owasp.png",
+      resumo: `conheça um pouco mais sobre essa entidade muito presente no fortalecimento da segurança de software.`,
     },
     {
       titulo: "Como recuperar Pendrive corrompido",
       categoria: "intermediario",
       url: "https://hackersec.com/como-recuperar-pendrive-corrompido/",
+      img: "img/informacao/intermediario/pendrive.jpg",
       resumo:
         "Vamos lhe ensinar como recuperar um pendrive corrompido de forma simples usando apenas o CMD com suporte ao formato USB para NTFS e FAT32.",
     },
@@ -77,6 +84,7 @@ var dados = {
       categoria: "intermediario",
       url:
         "https://www.kaspersky.com.br/resource-center/threats/computer-viruses-vs-worms",
+      img: "img/informacao/intermediario/virusWorm.jpg",
       resumo:
         "Saiba as diferenças entre esses dois tipos de programas maliciosos",
     },
@@ -85,6 +93,7 @@ var dados = {
       categoria: "avancado",
       url:
         "https://hackersec.com/as-certificacoes-de-ciberseguranca-mais-renomadas/",
+      img: "img/informacao/avancado/certificacoes.png",
       resumo: `Conhecer e procurar investir nas certificações de segurança cibernética mais renomadas é essencial para uma carreira de segurança da informação.
       Listamos as certificações mais renomadas e conhecidas do mercado nacional e internacional.`,
     },
@@ -93,12 +102,14 @@ var dados = {
       categoria: "avancado",
       url:
         "https://canaltech.com.br/internet/Como-configurar-uma-rede-sem-fio/",
+      img: "img/informacao/avancado/rede.png",
       resumo: `Comprou um roteador novo e não quer que ninguém acesso a sua rede sem fio? Confira todos os passos simples para proteger a sua internet e as configurações presentes na maioria dos modelos que são encontrados à venda para proteger o seu sinal.`,
     },
     {
       titulo: "Desafio Hacker: o que são os CTF",
       categoria: "avancado",
       url: "https://hackersec.com/desafios-hacker-o-que-sao-os-ctf/",
+      img: "img/informacao/avancado/CTF.jpeg",
       resumo: `Conhecidos como Capture The Flag e chamados de CTF, é basicamente uma competição hacker que apresenta diversos desafios de cibersegurança, onde o objetivo é concluir e pegar a “FLAG“, para poder pontuar. E podem ser jogados individualmente ou em equipe.
       Cada vez mais populares os CTFs são promovidos tanto na Internet como em eventos hacker e tecnologia em todo mundo. São sempre muito disputados geralmente por jovens que estão começando na área.`,
     },
@@ -106,6 +117,7 @@ var dados = {
       titulo: "Criptografia",
       categoria: "avancado",
       url: "https://hackersec.com/introducao-a-criptografia/",
+      img: "img/informacao/avancado/criptografia.jpg",
       resumo:
         "É quase impossível falar de segurança da informação e não vir à cabeça assuntos relacionados a criptografia, isso se deve ao fato da importância em possuir sistemas com criptografia na confidencialidade das informações, e é por este motivo, que preparamos esta matéria abordando os principais conceitos e fundamentos da criptografia.",
     },
@@ -113,6 +125,7 @@ var dados = {
       titulo: "O que é Pentest?",
       categoria: "avancado",
       url: "https://hackersec.com/o-que-e-pentest/",
+      img: "img/informacao/avancado/pentest.jpg",
       resumo:
         "Cada dia mais ouvimos falar em Pentest, porque o mundo conectado é uma realidade consolidada e irreversível, e a cada dia surgem novas soluções que visam promover um maior conforto, facilidade e agilidade no dia-a-dia das pessoas e empresas, especialmente serviços financeiros.",
     },
@@ -127,6 +140,7 @@ onload = () => {
   } else {
     login();
   }
+  atualizaTela();
 };
 
 let logado = () => {
@@ -147,4 +161,31 @@ let login = () => {
   for (i = 0; i < logins.length; i++) {
     logins[i].setAttribute("href", "login.html");
   }
+};
+
+let atualizaTela = () => {
+  let tela = document.querySelector(".cards-main");
+  let texto = "";
+  for (i = 0; i < dados.noticia.length; i++) {
+    let noticia = dados.noticia[i];
+
+    texto += `<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 cards">
+    <div class="card" style="width: 18rem;">
+    <a href="${noticia.url}"
+      ><img src="${noticia.img}" class="card-img-top padrao" alt="..."
+    /></a>
+    <div class="card-body">
+    <h5 class="card-title">${noticia.titulo}</h5>
+      <p class="card-text">
+        ${noticia.resumo}
+      </p>
+      <p class="more">
+        <a href="${noticia.url}"><small> Saber mais -></small></a>
+      </p>
+    </div>
+  </div>
+  </div>
+  `;
+  }
+  tela.innerHTML = texto;
 };
