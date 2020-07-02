1816ce92084733366723add329ee00307af976e4
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-07-02 16:58:27 -0300

    erro

--- minhaConta.js
+++ minhaConta.js
@@ -79,12 +79,18 @@ let salvarSenha = () => {
     login = document.getElementById("txtLogin").value;
     senha = document.getElementById("txtSenha").value;
 
+<<<<<<< Updated upstream
     salvas = JSON.parse(localStorage.getItem("senhasSalvas"));
+=======
+<<<<<<< HEAD
+    salvas = JSON.parse(localStorage.getItem('senhasSalvas'));
+>>>>>>> Stashed changes
 
     salvas.senhasSalvas.push({ endereco, login, senha });
 
     localStorage.setItem("senhasSalvas", JSON.stringify(salvas));
   }
+<<<<<<< Updated upstream
 };
 
 let exibeSenhasSalvas = () => {
@@ -113,6 +119,21 @@ let exibeSenhasSalvas = () => {
     texto =
       texto +
       `
+=======
+}
+=======
+    salvas = JSON.parse(localStorage.getItem("senhasSalvas"));
+>>>>>>> 01f2781b763248a9861242d909b05ae9c76d16b4
+
+    for (i = 0; i < salvas.senhasSalvas.length; i++) {
+      let endereco = salvas.senhasSalvas[i].endereco;
+      let login = salvas.senhasSalvas[i].login;
+      let senha = salvas.senhasSalvas[i].senha;
+
+      texto =
+        texto +
+        `
+>>>>>>> Stashed changes
     <li class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
       <section>${endereco}</section>
     </li>
@@ -123,6 +144,10 @@ let exibeSenhasSalvas = () => {
         <button type="button" value="${i}" class="btn btn-danger" data-dismiss="modal" onclick="apagaSenha(${i})">Apagar senha</button>
       </div>
     </div>`;
+<<<<<<< Updated upstream
+=======
+    }
+>>>>>>> Stashed changes
   }
 
   tela.innerHTML = texto;
@@ -163,6 +188,10 @@ onload = () => {
   if (usuarioCorrente != null) {
     logado();
     exibeSenhasSalvas();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     document.getElementById("logout").addEventListener("click", logout);
   } else {
     login();
