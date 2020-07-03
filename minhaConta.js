a7e0c8edf2620a61622da6927f175193f5e97f7b
joao amorim <amorimvictorjoao3@gmail.com>
2020-07-03 14:29:13 -0300

    favoritos finalizado

--- minhaConta.js
+++ minhaConta.js
@@ -29,8 +29,16 @@ let login = () => {
 };
 
 let encontraFavoritos = () => {
-  let favoritos = localStorage.getItem("favoritos");
-  atualizaTela(favoritos);
+  let favoritos = JSON.parse(localStorage.getItem("favoritos"));
+  let usuarioCorrente = JSON.parse(sessionStorage.getItem("usuarioCorrente"));
+  console.log(favoritos);
+  let favoritosDoUsuario = { favoritos: [] };
+
+  for (i = 0; i < favoritos.usuario.length; i++) {
+    if (favoritos.usuario[i].usuario == usuarioCorrente.login)
+      favoritosDoUsuario.favoritos.push(favoritos.usuario[i].card);
+  }
+  atualizaTela(favoritosDoUsuario.favoritos);
 };
 
 let atualizaTela = (conteudo) => {
@@ -40,22 +48,49 @@ let atualizaTela = (conteudo) => {
     let noticia = conteudo[i];
 
     texto += `<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 cards" id="${noticia.id}">
-      <div class="card" style="width: 18rem;">
-      <a href="${noticia.url}" target = "_blank"
-        ><img src="${noticia.img}" class="card-img-top padrao" alt="..."
-      /></a>
-      <div class="card-body">
-      <a href="${noticia.url}"><h5 class="card-title">${noticia.titulo}</h5></a>
-        <p class="card-text">
-          ${noticia.resumo}
-        </p>
-        <p class="more">
-          <a href="${noticia.url}"><small> Saber mais -></small></a>
-        </p>
+    <div class="card bg-dark" style="width: 18rem;">
+    <a href="${noticia.url}" target = "blank" rel="noopener noreferrer"
+      ><img src="${noticia.img}" class="card-img-top padrao" alt="..."
+    /></a>
+    <div class="card-body">
+    <a href="${noticia.url}" target="blank" rel="noopener noreferrer"><h5 class="card-title">${noticia.titulo}</h5></a>
+      <p class="card-text">
+        ${noticia.resumo}
+      </p>
       </div>
-    </div>
-    </div>
-    `;
+      <p class="more">
+      <div class = "card-footer">
+        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModal${i}">Saber mais</button>
+      </div>
+
+      <div class="modal fade" id="exampleModal${i}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+          <div class="modal-content text-white">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">${noticia.titulo}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <h5>${noticia.texto}</h5>
+            </div>
+            <div class="modal-footer">
+              <a href="${noticia.url}" target="blank" rel="noopener noreferrer">
+                <button type="button" class="btn btn-primary visitaSite" >Visitar Site</button>
+              </a>
+              <button type="button" class="btn btn-dark adicionaFavorito" id="${noticia.id}" onclick = "adicionaFavorito(this)"> Favoritar</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+            </p>
+          </div>
+        </div>
+  
+  
+  `;
   }
   tela.innerHTML = texto;
 };
@@ -69,45 +104,49 @@ let salvarSenha = () => {
     login = document.getElementById("txtLogin").value;
     senha = document.getElementById("txtSenha").value;
 
-    localStorage.setItem('senhasSalvas', JSON.stringify({ senhasSalvas: [{ endereco, login, senha }] }));
+    localStorage.setItem(
+      "senhasSalvas",
+      JSON.stringify({ senhasSalvas: [{ endereco, login, senha }] })
+    );
   } else {
     endereco = document.getElementById("txtEndereco").value;
     login = document.getElementById("txtLogin").value;
     senha = document.getElementById("txtSenha").value;
 
-    salvas = JSON.parse(localStorage.getItem('senhasSalvas'));
+    salvas = JSON.parse(localStorage.getItem("senhasSalvas"));
 
     salvas.senhasSalvas.push({ endereco, login, senha });
 
-    localStorage.setItem('senhasSalvas', JSON.stringify(salvas));
+    localStorage.setItem("senhasSalvas", JSON.stringify(salvas));
   }
-}
+};
 
 let exibeSenhasSalvas = () => {
   let tela = document.querySelector("#listaSenhas");
   let texto = "";
   let telaQt = document.querySelector("#qtSenhas");
   let textoQt = "";
-  
-  salvas = JSON.parse(localStorage.getItem('senhasSalvas'));
-  
+
+  salvas = JSON.parse(localStorage.getItem("senhasSalvas"));
+
   if (salvas.senhasSalvas.length === 0) {
-    localStorage.removeItem('pesquisasSalvas');
+    localStorage.removeItem("pesquisasSalvas");
   }
-  
+
   let qtSenhas = salvas["senhasSalvas"].length;
   textoQt = `
     <p>${qtSenhas} senha(s)</p>
   `;
   telaQt.innerHTML = textoQt;
-  
+
   for (i = 0; i < salvas.senhasSalvas.length; i++) {
     let endereco = salvas.senhasSalvas[i].endereco;
     let login = salvas.senhasSalvas[i].login;
     let senha = salvas.senhasSalvas[i].senha;
 
-
-    texto = texto + `
+    texto =
+      texto +
+      `
     <li class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
       <section>${endereco}</section>
     </li>
@@ -118,17 +157,17 @@ let exibeSenhasSalvas = () => {
         <button type="button" value="${i}" class="btn btn-danger" data-dismiss="modal" onclick="apagaSenha(${i})">Apagar senha</button>
       </div>
     </div>`;
-  };
+  }
 
   tela.innerHTML = texto;
 };
 
 let pesquisaSenhas = () => {
   var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('txtBusca');
+  input = document.getElementById("txtBusca");
   filter = input.value.toUpperCase();
   ul = document.getElementById("listaSenhas");
-  li = ul.getElementsByTagName('li');
+  li = ul.getElementsByTagName("li");
 
   for (i = 0; i < li.length; i++) {
     tag = li[i].getElementsByTagName("section")[0];
@@ -139,12 +178,12 @@ let pesquisaSenhas = () => {
       li[i].style.display = "none";
     }
   }
-}
+};
 
 let apagaSenha = (id) => {
-  salvas = JSON.parse(localStorage.getItem('senhasSalvas'));
+  salvas = JSON.parse(localStorage.getItem("senhasSalvas"));
   salvas.senhasSalvas.splice(id, 1);
-  localStorage.setItem('senhasSalvas', JSON.stringify(salvas));
+  localStorage.setItem("senhasSalvas", JSON.stringify(salvas));
   exibeSenhasSalvas();
 };
 
@@ -157,10 +196,11 @@ onload = () => {
   let usuarioCorrente = JSON.parse(sessionStorage.getItem("usuarioCorrente"));
   if (usuarioCorrente != null) {
     logado();
+    exibeSenhasSalvas();
+    encontraFavoritos();
 
     document.getElementById("logout").addEventListener("click", logout);
   } else {
     login();
   }
 };
-onload = exibeSenhasSalvas();
